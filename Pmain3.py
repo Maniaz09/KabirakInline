fb5ea97eb1864e7cb1654a89d75347651cce2756
Mani_az09 <71469042+Maniaz09@users.noreply.github.com>
2024-11-03 21:46:11 +0330

    Update Pmain3.py

--- Pmain3.py
+++ Pmain3.py
@@ -73,8 +73,8 @@ def video_downloader(update: M, url):
     update.reply_document(ytdlp_w.name)
     
     for fmt in formats:
-        if fmt["height"] == 480:
-            if fmt.get("acodec") and fmt.get("vcodec"):
+        if fmt.get(height) == 480:
+            if fmt.get("acodec") and fmt.get("vcodec") or not (fmt.get("acodec") or fmt.get("vcodec")):
                 dest_fmt = fmt["format_id"]
                 break
                 
